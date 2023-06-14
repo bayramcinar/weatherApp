b88c79af91f6996909dae22aa33479c275e491a2
Bayram Çınar <99193151+bayramcinar@users.noreply.github.com>
2023-06-14 23:26:46 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -6,17 +6,8 @@ var cloudy = document.getElementById("cloudy");
 var rainy = document.getElementById("rainy");
 var snowy = document.getElementById("snowy");
 var sunny = document.getElementById("sunny");
-var cloudyN = document.getElementById("cloudy-night");
-var rainyN = document.getElementById("rainy-night");
 var clearN = document.getElementById("clear-night");
-var videoRainy = document.getElementById("rainy");
-var videoSunny = document.getElementById("sunny");
-var videoSnowy = document.getElementById("snowy");
-var videoCloudy = document.getElementById("cloudy");
-var videoClear = document.getElementById("clear");
-var videoClearN = document.getElementById("clear-night");
-var videoRainyN = document.getElementById("rainy-night");
-var videoCloudyN = document.getElementById("cloudy-night");
+
 
 var search = document.getElementById("search");
 var havaLogo = document.querySelector(".havaLogo");
@@ -49,34 +40,36 @@ document.getElementById("sunny").style.display = "none";
 document.getElementById("snowy").style.display = "none";
 document.getElementById("clear").style.display = "none";
 document.getElementById("clear-night").style.display = "none";
-document.getElementById("rainy-night").style.display = "none";
-document.getElementById("cloudy-night").style.display = "none";
  
 
-if((timeString >= "20:00" && timeString <= "23:59") || (timeString >= "00:00" && timeString <= "05:00")){
-    if (data.current.condition.text.includes(("yağmur") || ("Yağmur")|| ("yağmurlu")|| ("Yağmurlu"))) {
-        rainyN.style.display = "block";
-    }else if (data.current.condition.text.includes(("Bulut") || ("bulut") || ("bulutlu") || ("Bulutlu"))) {
-        cloudyN.style.display = "block";
-    }else if (data.current.condition.text.includes("Açık")) {
-        clearN.style.display = "block";
-    }
-}
-else{
-    if (data.current.condition.text.includes(("yağmur") || ("Yağmur")|| ("yağmurlu")|| ("Yağmurlu"))) {
+
+    if (data.current.condition.text.includes("yağmur") || 
+    data.current.condition.text.includes("Yağmur") || 
+    data.current.condition.text.includes("yağmurlu") || 
+    data.current.condition.text.includes("Yağmurlu")) {
         rainy.style.display = "block";
-    } else if (data.current.condition.text.includes(("Bulut") || ("bulut") || ("bulutlu") || ("Bulutlu"))) {
+    }
+    else if (data.current.condition.text.includes("Bulut") || 
+            data.current.condition.text.includes("bulut") || 
+            data.current.condition.text.includes("bulutlu") || 
+            data.current.condition.text.includes("Bulutlu")) {
       cloudy.style.display = "block";
-    } else if (data.current.condition.text.includes(("Güneş") || ("güneş") || ("güneşli") || ("Güneşli"))) {
+    } else if (data.current.condition.text.includes("Güneş") || 
+    data.current.condition.text.includes("güneş") || 
+    data.current.condition.text.includes("güneşli") || 
+    data.current.condition.text.includes("Güneşli")) {
       sunny.style.display = "block";
     } else if (data.current.condition.text.includes("Kar")) {
       snowy.style.display = "block";
     } else if (data.current.condition.text.includes("Açık")) {
       clear.style.display = "block";
-    }else if (data.current.condition.text.includes(("Yağışlı") || ("yağışlı") || ("yağış") || ("Yağış"))){
+    }else if (data.current.condition.text.includes("Yağışlı") ||
+    data.current.condition.text.includes("yağışlı") ||
+    data.current.condition.text.includes("yağış") ||
+    data.current.condition.text.includes("Yağış")){
         rainy.style.display = "block";
     }
-}
+
 
 })
 .catch(error => {
@@ -89,135 +82,58 @@ function submitForm(event) {
     const apiUrl = `http://api.weatherapi.com/v1/current.json?key=08ea5cc9e68f402c9af142027231206&q=${inputValue}&aqi=no&lang=tr`;
   
     fetch(apiUrl)
-      .then(response => response.json())
-      .then(data => {
-        time = data.location.localtime;
-        var timeString = time.split(" ")[1];
-        if((timeString >= "20:00" && timeString <= "23:59") || (timeString >= "00:00" && timeString <= "05:00")){
-            if (data.current.condition.text.includes(("yağmur") || ("Yağmur")|| ("yağmurlu")|| ("Yağmurlu"))) {
-                rainyN.style.display = "block";
-            }else if (data.current.condition.text.includes(("Bulut") || ("bulut") || ("bulutlu") || ("Bulutlu"))) {
-                cloudyN.style.display = "block";
-            }else if (data.current.condition.text.includes("Açık")) {
-                clearN.style.display = "block";
-            }
+    .then(response => response.json())
+    .then(data => {
+    durum.innerHTML = data.current.condition.text;
+    city1.innerHTML = data.location.name;
+    hava.innerHTML = data.current.feelslike_c + " C°";
+    nem.innerHTML = "Nem : " + data.current.humidity;
+    rüzgar.innerHTML = "Rüzgar Hızı : " + data.current.wind_mph + " m/s";
+    havaLogo.src = data.current.condition.icon;
+    time = data.location.localtime;
+    var timeString = time.split(" ")[1];
+
+      
+    document.getElementById("rainy").style.display = "none";
+    document.getElementById("cloudy").style.display = "none";
+    document.getElementById("sunny").style.display = "none";
+    document.getElementById("snowy").style.display = "none";
+    document.getElementById("clear").style.display = "none";
+    document.getElementById("clear-night").style.display = "none";
+    
+
+
+        if (data.current.condition.text.includes("yağmur") || 
+        data.current.condition.text.includes("Yağmur") || 
+        data.current.condition.text.includes("yağmurlu") || 
+        data.current.condition.text.includes("Yağmurlu")) {
+            rainy.style.display = "block";
         }
-        else{
-            if (data.current.condition.text.includes(("yağmur") || ("Yağmur")|| ("yağmurlu")|| ("Yağmurlu"))) {
-                rainy.style.display = "block";
-            } else if (data.current.condition.text.includes(("Bulut") || ("bulut") || ("bulutlu") || ("Bulutlu"))) {
-              cloudy.style.display = "block";
-            } else if (data.current.condition.text.includes(("Güneş") || ("güneş") || ("güneşli") || ("Güneşli"))) {
-              sunny.style.display = "block";
-            } else if (data.current.condition.text.includes("Kar")) {
-              snowy.style.display = "block";
-            } else if (data.current.condition.text.includes("Açık")) {
-              clear.style.display = "block";
-            }else if (data.current.condition.text.includes(("Yağışlı") || ("yağışlı") || ("yağış") || ("Yağış"))){
-                rainy.style.display = "block";
-            }
+        else if (data.current.condition.text.includes("Bulut") || 
+                data.current.condition.text.includes("bulut") || 
+                data.current.condition.text.includes("bulutlu") || 
+                data.current.condition.text.includes("Bulutlu")) {
+          cloudy.style.display = "block";
+        } else if (data.current.condition.text.includes("Güneş") || 
+        data.current.condition.text.includes("güneş") || 
+        data.current.condition.text.includes("güneşli") || 
+        data.current.condition.text.includes("Güneşli")) {
+          sunny.style.display = "block";
+        } else if (data.current.condition.text.includes("Kar")) {
+          snowy.style.display = "block";
+        } else if (data.current.condition.text.includes("Açık")) {
+          clear.style.display = "block";
+        }else if (data.current.condition.text.includes("Yağışlı") ||
+        data.current.condition.text.includes("yağışlı") ||
+        data.current.condition.text.includes("yağış") ||
+        data.current.condition.text.includes("Yağış")){
+            rainy.style.display = "block";
         }
-        durum.innerHTML = data.current.condition.text;
-        city1.innerHTML = data.location.name;
-        hava.innerHTML = data.current.feelslike_c + " C°";
-        nem.innerHTML = "Nem : " + data.current.humidity;
-        rüzgar.innerHTML = "Rüzgar Hızı : " + data.current.wind_mph + " m/s";
-        havaLogo.src = data.current.condition.icon;
-  
-        document.getElementById("rainy").style.display = "none";
-        document.getElementById("cloudy").style.display = "none";
-        document.getElementById("sunny").style.display = "none";
-        document.getElementById("snowy").style.display = "none";
-        document.getElementById("clear").style.display = "none";
-        document.getElementById("clear-night").style.display = "none";
-        document.getElementById("rainy-night").style.display = "none";
-        document.getElementById("cloudy-night").style.display = "none";
-      })
-      .catch(error => {
-        // Hata yönetimi
+
+    })
+    .catch(error => {
         console.log("API isteği sırasında bir hata oluştu:", error);
-      });
+    });
   }
 
 
-// request.addEventListener("load", function(){
-//     const data = JSON.parse(this.responseText)
-//     console.log(data);
-//     durum.innerHTML = data.current.condition.text
-//     city1.innerHTML = data.location.name;
-//     hava.innerHTML = data.current.feelslike_c;
-//     if((data.current.condition.text).includes("rain")){
-//         rainy.style.display = "block";
-//     }
-//     else if((data.current.condition.text).includes("cloudy")){
-//         cloudy.style.display = "block";
-//     }
-//     else if((data.current.condition.text).includes("Sunny")){
-//         sunny.style.display = "block";
-//     }
-//     else if((data.current.condition.text).includes("snowy")){
-//         snowy.style.display = "block";
-//     }
-//     else if((data.current.condition.text).includes("clear")){
-//         clear.style.display = "block";
-//     }
-// });
-function playVideoRainy() {
-    videoRainy.play();
-}
-
-videoRainy.addEventListener("ended", function () {
-  this.currentTime = 0;
-  this.play();
- 
-});
-
-playVideoRainy();
-
-
-function playVideoSunny() {
-    videoSunny.play();
-}
-
-videoSunny.addEventListener("ended", function () {
-  this.currentTime = 0;
-  this.play();
-});
-
-playVideoSunny();
-
-
-function playVideoSnowy() {
-    videoSnowy.play();
-}
-
-videoSnowy.addEventListener("ended", function () {
-  this.currentTime = 0;
-  this.play();
-});
-
-playVideoSnowy();
-
-
-function playVideoCloudy() {
-    videoCloudy.play();
-}
-
-videoCloudy.addEventListener("ended", function () {
-  this.currentTime = 0;
-  this.play();
-});
-
-playVideoCloudy();
-
-
-function playVideoClear() {
-    videoClear.play();
-}
-
-videoClear.addEventListener("ended", function () {
-  this.currentTime = 0;
-  this.play();
-});
-
-playVideoClear();
